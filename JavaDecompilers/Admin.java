2a67ac413b1f6322216b3d06c15a37d166f4c86d
AlexandruPanainte <61983928+AlexandruPanainte@users.noreply.github.com>
2020-03-19 12:07:07 -0700

    JavaDecompilers

---
+++ JavaDecompilers/Admin.java
@@ -0,0 +1,44 @@
+// Decompiled by DJ v3.8.8.85 Copyright 2005 Atanas Neshkov  Date: 3/19/2020 12:04:29 PM
+// Home Page : http://members.fortunecity.com/neshkov/dj.html  - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Admin.java
+
+package com.company;
+
+
+// Referenced classes of package com.company:
+//            User
+
+public class Admin extends User
+{
+
+    public Admin()
+    {
+    }
+
+    public void CreateUser(User user1)
+    {
+    }
+
+    public void deleteUser(User user1)
+    {
+    }
+
+    public void deleteMessage(String s)
+    {
+    }
+
+    public void login(String s, String s1)
+    {
+    }
+
+    public void writeMessage(String s)
+    {
+    }
+
+    public String lastName;
+    public String firstName;
+    public String id;
+    public String username;
+    public String password;
+}
---
+++ JavaDecompilers/User.java
@@ -0,0 +1,29 @@
+// Decompiled by DJ v3.8.8.85 Copyright 2005 Atanas Neshkov  Date: 3/19/2020 12:04:29 PM
+// Home Page : http://members.fortunecity.com/neshkov/dj.html  - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   User.java
+
+package com.company;
+
+
+public abstract class User
+{
+
+    public User()
+    {
+    }
+
+    public void login(String s, String s1)
+    {
+    }
+
+    public void writeMessage(String s)
+    {
+    }
+
+    public String firstName;
+    public String lastName;
+    public String id;
+    public String username;
+    public String password;
+}
